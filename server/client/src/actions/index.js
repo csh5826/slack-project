a6a35c751a1bc7a36e2150494322160ada6dbf07
PJ Foley <pjfoley@gmail.com>
2020-10-20 10:49:55 -0700

    receiving api changes

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const SET_CURRENT_USER = 'set_current_user';
 
 export function setCurrentUser(user) {
-axios.post(`http://localhost:5000/login/`).then(function (response) {
+axios.post(`http://localhost:5000/api/login/`).then(function (response) {
   console.log('received from server-login: ', response.data)
 })
 .catch(function (error) {console.log(error);})  
