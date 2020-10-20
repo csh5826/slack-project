3be08c8c44670ec335c3db18442fc4ddef3f0297
Aissa Feldmann <aissafeldmann@Aissas-Air.attlocal.net>
2020-10-19 21:36:58 -0400

    server up on 5000, login route good; channels route is 404

--- server/index.js
+++ server/index.js
@@ -1,31 +1,48 @@
 const express = require('express');
-//add what we need for postgres here
-// const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-// mongoose.connect('mongodb://localhost/products', { useNewUrlParser: true });
+const channels = require('./routes/channels');
+const login = require('./routes/login');
+const root = require('./routes/login');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(function( req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   next();
 });
 
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({extended: false}));
 
-const mainRoutes = require('./router');
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
 
-app.use(mainRoutes);
+app.use('/', root);
+app.use('/login', login);
+app.use('/channels', channels);
 
-app.listen(8000, () => {
-    console.log('Node.js listening on port ' + 8000);
-  });
+//add what we need for postgres here
+// const mongoose = require('mongoose');
+// mongoose.connect(keys.MONGODB_URI);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
   
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+let port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
