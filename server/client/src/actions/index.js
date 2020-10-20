d417610c96832c7ad78bcc1dd45bd5a41b9ef150
PJ Foley <pjfoley@gmail.com>
2020-10-20 07:56:21 -0700

    Round trip to login endpoint successful

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -3,7 +3,10 @@ import axios from "axios";
 export const SET_CURRENT_USER = 'set_current_user';
 
 export function setCurrentUser(user) {
-  
+axios.post(`http://localhost:5000/login/`).then(function (response) {
+  console.log('received from server-login: ', response.data)
+})
+.catch(function (error) {console.log(error);})  
   return {
     type: SET_CURRENT_USER,
     payload: user
