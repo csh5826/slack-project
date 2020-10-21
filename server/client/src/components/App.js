e67f3bb2c4645043cba5e16d95e456924ce59d4a
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-21 11:21:40 -0400

    initial commit for implementing sockets

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-// import { useState } from "react";
+import { useState, useEffect } from "react";
+import socketio from 'socket.io-client';
+import TextField from '@material-ui/core/TextField';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Row, Container, ListGroup, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
+const server = 'http://localhost:5000/'
+
+const io = socketio(server);
+    io.on('connection', () => {
+      console.log('we are connected with the backend')
+    })  
 
 const App = (props) => {
   console.log("the props you need  ",  props);
@@ -12,6 +20,39 @@ const App = (props) => {
   const logoutClicked = (event) => {
     console.log('logout button clicked');
   };
+
+  //defining state
+const [state, setState] = useState({name: props.loggedInUser.username, message: ''})
+const [chat, setChat] = useState([])
+
+
+// socket listening for our message action, setchat is pulling all previous chat and making sure it is displayed
+useEffect(() => {
+    io.on('message', ({name, message}) => {
+        setChat([...chat, {name, message}])
+    })
+})
+const renderChat = () => {
+    return chat.map(({name, message}, index) => (
+        <div key={index}>
+            <h3>{name}: <span>{message}</span></h3>
+        </div>
+    ))
+}
+
+const onTextChange = (e) => {
+    setState({...state, [e.target.name]: e.target.value})
+    console.log(state)
+}
+
+const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const {name, message} = state
+    io.emit('message', {name, message})
+    setState({message: '', name})
+    
+
+}
 
   return (
     <Container fluid>
@@ -41,15 +82,14 @@ const App = (props) => {
     <Col sm={9}>
     <Button variant="outline-dark" size="sm" className="float-right" onClick={logoutClicked}>Logout</Button>
     <h4>General Channel</h4>
-    <div className="chat-window" style={{background: 'snow', height: '90%'}}><ListGroup variant="flush">
-      <ListGroup.Item style={{background: 'snow'}}><b>PJ</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>WordsWords blahblah stuffftuff meaningless stuff</ListGroup.Item>
-      <ListGroup.Item style={{background: 'snow'}}><b>Jim</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>Things and other things and stuff</ListGroup.Item>
-      <ListGroup.Item style={{background: 'snow'}}><b>Daniel</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>More things than that!</ListGroup.Item>
-      <ListGroup.Item style={{background: 'snow'}}><b>Aissa</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>And nicer things.</ListGroup.Item>
-      <ListGroup.Item style={{background: 'snow'}}><b>PJ</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>So many things</ListGroup.Item>
+    <div className="chat-window" style={{background: 'snow', height: '90%'}}>
+      <ListGroup variant="flush">
+        {renderChat()}
+      
       </ListGroup>
     </div><div className="message-composer" style={{background: 'silver', height: 'auto'}}><InputGroup>
-    <FormControl as="textarea" aria-label="text area" /><InputGroup.Append><InputGroup.Text onClick={postMessage}>{props.loggedInUser.username}</InputGroup.Text></InputGroup.Append></InputGroup></div>
+    <form onSubmit={onMessageSubmit}><TextField name='message'onChange = {e => onTextChange(e)} value={state.message}label='message' /></form>
+    <InputGroup.Append><InputGroup.Text onClick={postMessage}>{props.loggedInUser.username}</InputGroup.Text></InputGroup.Append></InputGroup></div>
     </Col>
     </Row>
   </Container>
@@ -70,3 +110,10 @@ function mapStateToProps(state) {
 // }
 
 export default connect(mapStateToProps) (App);
+
+
+{/* <ListGroup.Item style={{background: 'snow'}}><b>PJ</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>WordsWords blahblah stuffftuff meaningless stuff</ListGroup.Item>
+      <ListGroup.Item style={{background: 'snow'}}><b>Jim</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>Things and other things and stuff</ListGroup.Item>
+      <ListGroup.Item style={{background: 'snow'}}><b>Daniel</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>More things than that!</ListGroup.Item>
+      <ListGroup.Item style={{background: 'snow'}}><b>Aissa</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>And nicer things.</ListGroup.Item>
+      <ListGroup.Item style={{background: 'snow'}}><b>PJ</b>  <i style={{color: 'lightgrey', fontSize: 'smaller'}}> time 12:34pm </i>So many things</ListGroup.Item> */}
