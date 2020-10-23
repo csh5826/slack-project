4a5fcaedf4358f71ace5790ada10157bfe3a31d1
PJ Foley <pjfoley@gmail.com>
2020-10-23 07:55:10 -0700

    where is my sql file

---
+++ server/client/src/components/__Loginas_func.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { setCurrentUser } from '../actions';
+import { Row, Container, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
+
+
+
+ 
+//class Login extends Component {
+const Login = (props) => {
+const submitLogin = (event) => {
+  console.log(event.target.value);
+  if (event.key === "Enter") {
+    props.setCurrentUser(event.target.value);
+    //insert delay or promise here?
+    props.history.push('/');
+  }
+  //   
+  // this.props.signin(formProps, () => {
+ //     this.props.history.push('/');
+  //   });
+  };
+  //render() {
+
+
+  return (
+    <Container fluid>
+      <Row>&nbsp;</Row>
+      <Row>
+        <Col></Col>
+      <Col md="auto">
+      <h3>Enter your name to join:</h3>
+      <InputGroup className="mb-3" onKeyUp={submitLogin} name="login-form"><FormControl placeholder="Your name"></FormControl></InputGroup>
+      <Button variant="outline-dark" onClick={submitLogin} className="float-right">Login</Button>
+      </Col>
+      <Col></Col>
+      </Row>
+    </Container>
+          
+        
+  );
+}
+//}
+
+function mapStateToProps(state) {
+  return { loggedInUser: state.loggedinUser}
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      setCurrentUser
+    },
+    dispatch
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
+ 
